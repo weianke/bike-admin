bad987e05bf8625e1cca75bb433be22673a3ecce
anke <245783065@qq.com>
2018-09-16 21:49:55 +0800

    add

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Life from './pages/demo/life';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render( < Life / > , document.getElementById('root'));
 registerServiceWorker();
