332096071a0e3e0f6513fd2235c5f204e1850945
anke <245783065@qq.com>
2018-09-16 21:52:31 +0800

    remover history

--- .history/src/index_20180916152643.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Life from './pages/demo/life';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render( < Life / > , document.getElementById('root'));
-registerServiceWorker();
