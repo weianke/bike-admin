9c2246a4708eb5905f1109ec8191317489adf5b4
weinake <245383065@qq.com>
2018-10-07 21:48:15 +0800

    add

--- src/pages/demo/life.js
+++ src/pages/demo/life.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Child from './child'
+import './index.css'
 
 export default class Life extends React.Component {
   constructor(props) {
@@ -25,7 +26,7 @@ export default class Life extends React.Component {
     var style = {
       padding: 50
     }
-    return <div style = {style} >
+    return <div className="content">
       <p>安可</p>
       <button onClick={this.handleAdd}>点击一下</button>
       < button onClick = {this.handleClick.bind(this)} >点击一下</button>
