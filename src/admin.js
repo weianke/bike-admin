b431fa4c04f8499e1eafbe1378690e2a7735641b
weinake <245383065@qq.com>
2018-10-21 18:24:01 +0800

    目录结构

---
+++ src/admin.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Row, Col } from 'antd';
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+export default class Admin extends React.Component{
+
+    render () {
+        return (
+            <Row>
+                <Col span="3">
+                    Left
+                </Col>
+                <Col span="21">
+                    <Header>
+                        Header
+                    </Header>
+                    <Row>
+                        content
+                    </Row>
+                    <Footer>
+                        Footer
+                    </Footer>
+                </Col>
+            </Row>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Life from './pages/demo/life';
+import Admin from './admin';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render( < Life / > , document.getElementById('root'));
+ReactDOM.render( < Admin / > , document.getElementById('root'));
 registerServiceWorker();
