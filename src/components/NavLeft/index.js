39f2cb80c8a3ccc73f48b1efdc1111a65e472af0
anke <245783065@qq.com>
2018-10-22 12:33:35 +0800

    navlist

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import MenuConfig from '../../config/menuConfig'
-import { Menu, Icon } from 'antd';
+import { Menu } from 'antd';
 import './index.less'
 const SubMenu = Menu.SubMenu;
 
 export default class NavLeft extends React.Component {
+    componentWillMount(){
+      const menuTreeNode =  this.renderMenu(MenuConfig);
 
+      this.setState({
+        menuTreeNode
+      })
+    }
+    // 菜单渲染
+    renderMenu = (data) => {
+      return data.map((item) => {
+        // 判断如果有下层子节点，继续递归遍历子节点
+        if (item.children) {
+          return (
+            <SubMenu title={item.title} key={item.key}>
+              {this.renderMenu(item.children)}
+            </SubMenu>
+          )
+        }
+        return <Menu.Item  key={item.key}>{item.title}</Menu.Item>
+      })
+    }
     render() {
-
         return (
             <div>
                 <div className="logo">
@@ -15,12 +34,7 @@ export default class NavLeft extends React.Component {
                     <h1>Bike Admin</h1>
                 </div>
                 <Menu theme="dark">
-                    <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
-                        <Menu.Item key="1">Option 1</Menu.Item>
-                        <Menu.Item key="2">Option 2</Menu.Item>
-                        <Menu.Item key="3">Option 3</Menu.Item>
-                        <Menu.Item key="4">Option 4</Menu.Item>
-                    </SubMenu>
+                   {this.state.menuTreeNode}
                 </Menu>
             </div>
         )
