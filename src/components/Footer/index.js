23d2e388108b6d355219c7ffd82a347cc8aeb5ef
anke <245783065@qq.com>
2018-10-22 18:19:02 +0800

    fotter

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import './index.less'
 
 export default class Footer extends React.Component{
 
     render(){
         return (
-            <div>This is Footer</div>
+            <div className="footer">
+              版权所有：安可 请联系245783065@qq.com (推荐使用谷歌浏览器，可以获得更加操作页面的体验)
+            </div>
         )
     }
 }
