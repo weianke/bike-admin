6559e1474ebd05448c01b569bb60992aab780aad
weinake <245383065@qq.com>
2018-10-22 22:33:33 +0800

    content

---
+++ src/pages/home/index.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import './index.less'
+
+export default class Home extends React.Component{
+
+    render(){
+        return (
+            <div className="home-wrap">
+                欢迎来到Bike Aadmin共享单车后台管理系统
+            </div>
+        )
+    }
+}
