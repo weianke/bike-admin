6408ba5ffa3c037f004086bd0c8f12d8e6ae5fcc
anke <245783065@qq.com>
2018-10-22 17:48:46 +0800

    time

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,6 +4,7 @@
       if(!time)return '';
       let date = new Date(time);
       let minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
-      return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ':' + minutes + ':' + date.getSeconds();
+      let getSeconds = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
+      return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ':' + minutes + ':' + getSeconds;
    }
  }
