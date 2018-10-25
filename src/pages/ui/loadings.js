ab76012e50a276e68d61c404c9f2ca537cfc4fee
anke <245783065@qq.com>
2018-10-25 17:24:43 +0800

    Spin

--- src/pages/ui/loadings.js
+++ src/pages/ui/loadings.js
@@ -1,14 +1,41 @@
 import React from 'react'
-import {Card, Button, Spin} from 'antd'
+import {Card, Spin, Icon, Alert} from 'antd'
 
 export default class Loadings  extends React.Component{
 
     render(){
+        const icon = <Icon type="loading"   style={{fontSize: 24}}/>
+        const iconLoading = <Icon type="loading"   style={{fontSize: 24}}/>
         return (
              <div>
                  <Card title="Spin用法" className="card-wrap">
-                    <Spin/>
+                    <Spin size="small"/>
+                    <Spin style={{margin: '0 10px'}}/>
+                    <Spin size="large"/>     
+                    <Spin indicator={icon} style={{marginLeft: 10}}/>               
                  </Card>
+                 <Card title="内容遮罩">                    
+                          <Alert
+                            message="React"
+                            description="欢迎来到安可的网站"
+                            type="info"
+                          />
+                       <Spin tip="Loading" indicator={iconLoading}>
+                           <Alert
+                            message="React"
+                            description="欢迎来到安可的网站"
+                            type="warning"
+                          />
+                       </Spin>
+                       <Spin tip="Loading">
+                          <Alert
+                              message="React"
+                              description="欢迎来到安可的网站"
+                              type="success"
+                            />
+                      </Spin>
+                 </Card>
+                
              </div>
         );
     }
