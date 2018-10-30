0a53d2855d83323f2d32f39d96c908d10101514d
anke <245783065@qq.com>
2018-10-30 18:00:34 +0800

    basictable

--- src/axios/index.js
+++ src/axios/index.js
@@ -19,6 +19,11 @@ export default class Axios{
   }
 
   static ajax(options){
+    let loading;
+    if (options.data && options.data.isShowLoading !== false ) {
+       loading = document.getElementById('ajaxLoading');
+       loading.style.display = 'block';
+    }
     let baseUrl = 'https://www.easy-mock.com/mock/5bd7fc702f07612e0212412d/mockapi';
     return new Promise((resolve, reject) => {
         axios({
@@ -28,6 +33,10 @@ export default class Axios{
             timeout: 10000,
             params: (options.data && options.data.params) || ''
         }).then((response)=> {
+          if (options.data && options.data.isShowLoading !== false) {
+            loading = document.getElementById('ajaxLoading');
+            loading.style.display = 'none';
+          }
           if(response.status === 200){
              let res = response.data;
              if(res.code === 0){
