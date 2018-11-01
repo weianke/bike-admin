5a4de4fb6e585c024fd4dd463cd695e1870f838d
anke <245783065@qq.com>
2018-11-01 16:13:05 +0800

    high

--- src/pages/table/highTable.js
+++ src/pages/table/highTable.js
@@ -59,7 +59,7 @@ export default class HightTable extends React.Component{
   request = () => {
      let _this = this;
       axios.ajax({
-          url: '/table/list',
+          url: '/table/high/list',
           data: {
             params: {
                 page: this.params.page
@@ -86,10 +86,6 @@ export default class HightTable extends React.Component{
 
   onRowClick = (record, index) => {
     console.log(record, index)
-      // Modal.info({
-      //   title: '信息',
-      //   content: `用户名: ${record.userName}, 用户爱好：${record.interest}`
-      // })
       let selectKey = [index];
       this.setState({
         selectedRowKeys: selectKey,
@@ -308,6 +304,78 @@ export default class HightTable extends React.Component{
             dataIndex: 'time'
           }
     ];
+    const columns3 = [{
+        title: 'id',
+        width: 80,
+        dataIndex: 'id'
+      },
+      {
+        title: '用户名',
+        width: 80,
+        dataIndex: 'userName'
+      },
+      {
+        title: '年龄',
+        width: 80,
+        dataIndex: 'age'
+      },
+      {
+        title: '性别',
+        width: 80,
+        dataIndex: 'sex',
+        render(sex) {
+          return sex === 1 ? '男' : '女';
+        }
+      },
+      {
+        title: '状态',
+        width: 80,
+        dataIndex: 'state',
+        render(state) {
+          let config = {
+            '1': '咸鱼一条',
+            '2': '风华浪子',
+            '3': '北大才子',
+            '4': '百度FE',
+            '5': '创业者'
+          }
+          return config[state];
+        }
+      },
+      {
+        title: '爱好',
+        width: 80,
+        dataIndex: 'interest',
+        render(interest) {
+          let config = {
+            '1': '游泳',
+            '2': '打篮球',
+            '3': '踢足球',
+            '4': '跑步',
+            '5': '爬山',
+            '6': '骑行',
+            '7': '桌球',
+            '8': '麦霸'
+          }
+          return config[interest];
+        }
+      },
+      {
+        title: '生日',
+        width: 120,
+        dataIndex: 'birthday'
+      },
+      {
+        title: '地址',
+        width: 120,
+        dataIndex: 'address'
+      },
+      {
+        title: '早起时间',
+        width: 80,
+        dataIndex: 'time'
+      }
+    ];
     const rowSelection = {
         type: 'radio',
         selectedRowKeys: this.state.selectedRowKeys
@@ -338,6 +406,14 @@ export default class HightTable extends React.Component{
                <Table 
                     bordered
                     columns={columns2}
+                    dataSource={this.state.dataSource2}
+                    scroll={{x: 1920}}
+               />
+            </Card>
+             <Card title="表格排序" style={{marginTop: 10}}>
+               <Table 
+                    bordered
+                    columns={columns3}
                     dataSource={this.state.dataSource2}
                     scroll={{x: 1920}}
                />
