d65b339513db486aa92d7a910b04d6caa0e880ec
anke <245783065@qq.com>
2018-11-06 10:26:51 +0800

    order

---
+++ src/pages/order/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Card, Button, Table, Form, Select, Modal, message} from 'antd'
+import axios from './../../axios/index'
+import Utils from './../../utils/utils'
+
+export default class Order extends React.Component{
+
+    state = {}
+    render(){
+      return(
+          <div>
+
+          </div>
+      );
+    }
+}
