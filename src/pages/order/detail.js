a15f1a1fce9c6689247afec26e2bc07fa0c33ec8
anke <245783065@qq.com>
2018-11-07 17:43:55 +0800

    map

--- src/pages/order/detail.js
+++ src/pages/order/detail.js
@@ -30,15 +30,20 @@ export default class Order extends React.Component {
             this.setState({
               orderInfo: res.result
             })
-            _this.renderMap();
+            _this.renderMap(res.result);
          }
       })
    }
 
-   renderMap = () => {
+   renderMap = (result) => {
       this.map = new window.BMap.Map('orderDetailMap');
-      this.map.centerAndZoom('北京',11);
+      // this.map.centerAndZoom('北京',11);
+      // 添加地图控件
       this.addMapControl();
+      // 调用路线图绘制方法 并把接口返回的坐标传入绘制 
+      this.drawBikeRoute(result.position_list);
+      // 绘制中心服务区
+      this.drawServiceArea(result.area);
    }
 
 
@@ -51,6 +56,79 @@ export default class Order extends React.Component {
        map.addControl(new window.BMap.NavigationControl({
         anchor: window.BMAP_ANCHOR_BOTTOM_LEFT
       }))
+      map.enableScrollWheelZoom(true);
+   }
+   // 绘制用户的行驶路线
+   drawBikeRoute = (positionList) => {
+      let map = this.map;
+      let startPonit = '';
+      let endPonit = '';
+
+      if (positionList.length > 0) {
+        let first = positionList[0];
+        let last = positionList[positionList.length -1];
+         startPonit = new window.BMap.Point(first.lon, first.lat);
+         let startIcon = new window.BMap.Icon('/assets/start_point.png', new window.BMap.Size(36, 42), {
+           imageSize: new window.BMap.Size(36, 42),
+           anchor: new window.BMap.Size(36, 42)
+         })
+
+         // icon需要依赖于marker
+         let startMarker = new window.BMap.Marker(startPonit,{icon: startIcon});
+         // 开始坐标icon
+         this.map.addOverlay(startMarker);
+
+          endPonit = new window.BMap.Point(last.lon, last.lat);
+         let endIcon = new window.BMap.Icon('/assets/end_point.png', new window.BMap.Size(36, 42), {
+           imageSize: new window.BMap.Size(36, 42),
+           anchor: new window.BMap.Size(36, 42)
+         })
+
+         // icon需要依赖于marker
+         let endMarker = new window.BMap.Marker(endPonit,{icon: endIcon});
+         // 结束坐标icon
+         this.map.addOverlay(endMarker);
+
+         // 连接路线图
+         let trackPoint = [];
+         for (let i = 0; i < positionList.length; i++) {
+            let point = positionList[i];
+            trackPoint.push(new window.BMap.Point(point.lon, point.lat));
+         }
+
+        let polyline =  new window.BMap.Polyline(trackPoint, {
+            strokeColor: '#1869AD',
+            strokeWeight: 3,
+            strokeOpacity: 1
+         });
+
+         this.map.addOverlay(polyline);
+        
+         // 设置中心坐标点
+         this.map.centerAndZoom(endPonit, 11);
+      }
+     
+   }
+
+       // 绘制服务区
+   drawServiceArea = (positionList) => {
+          // 连接路线图
+          let trackPoint = [];
+          for (let i = 0; i < positionList.length; i++) {
+            let point = positionList[i];
+            trackPoint.push(new window.BMap.Point(point.lon, point.lat));
+          }
+
+        let polygon = new window.BMap.Polygon(trackPoint, {
+          strokeColor: '#CE0000',
+          strokeWeight: 4,
+          strokeOpacity: 1,
+          fillColor: '#ff8605',
+          fillOpacity: 0.4
+        })
+
+         this.map.addOverlay(polygon);
+
    }
 
    render(){
