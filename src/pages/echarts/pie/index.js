b4d71dcc9478299c7580eb25d72d57123d67dfc5
anke <245783065@qq.com>
2018-11-16 15:30:48 +0800

    echart

--- src/pages/echarts/pie/index.js
+++ src/pages/echarts/pie/index.js
@@ -144,6 +144,79 @@ export default class Bar extends React.Component {
         
     }
 
+
+        getOption3(){
+        let option = {     
+            title: {
+                text: '用户骑行订单',
+                x: 'center'
+            },
+            legend: {
+                orient: 'vertical',
+                right: 10,
+                top: 20,
+                bottom: 20,
+                data: [
+                    '周一',
+                    '周二',
+                    '周三',
+                    '周四',
+                    '周五',
+                    '周六',
+                    '周日'
+                ]
+            },
+            tooltip: {
+                trigger: 'item',
+                formatter: "{a} <br/>{b} : {c} ({d}%)"
+            },
+            series: [
+                {
+                    name: '订单量',
+                    type: 'pie',
+                    center: [
+                        '50%', '60%'
+                    ],
+                    data: [
+                        {
+                            value: 1000,
+                            name: '周一'
+                        }, {
+                            value: 1000,
+                            name: '周二'
+                        }, {
+                            value: 2000,
+                            name: '周三'
+                        }, {
+                            value: 1500,
+                            name: '周四'
+                        }, {
+                            value: 3000,
+                            name: '周五'
+                        }, {
+                            value: 2000,
+                            name: '周六'
+                        }, {
+                            value: 1200,
+                            name: '周日'
+                        }
+                    ].sort((a, b) => {
+                      return a.value - b.value;
+                    }),
+                    roseType: 'radius',
+                    itemStyle: {
+                        emphasis: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }
+            ]
+        }
+        return option;
+        
+    }
   render (){
     return (
       <div>
@@ -156,6 +229,12 @@ export default class Bar extends React.Component {
         </Card>
          <Card title="饼形图表之二" style={{marginTop: 10}}>
              <ReactEcharts option={this.getOption2()}  notMerge={true} lazyUpdate={true}
+                          theme="bike"
+                          style={{height: 500}}
+            />
+        </Card>
+          <Card title="饼形图表之三" style={{marginTop: 10}}>
+             <ReactEcharts option={this.getOption3()}  notMerge={true} lazyUpdate={true}
                           theme="bike"
                           style={{height: 500}}
             />
